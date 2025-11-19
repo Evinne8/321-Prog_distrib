f9e698564c0797518baf21e9cf1c6d5c10ed7a83
Evinne8 <pe41bnd@eduvaud.ch>
2025-11-19 10:58:43 +0100

    code(transat): Exercice réussi

---
+++ personnel/transat/Transat/FaillibleQos0Storage.cs
@@ -0,0 +1,33 @@
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+
+namespace Transat;
+
+public sealed class FaillibleQos0Storage
+{
+    private volatile int _dataCounter = 0;
+    private static readonly Random RandomGenerator = new();
+
+    private readonly ConcurrentDictionary<string, int> _data = new();
+
+    public ImmutableList<int> Values => _data.Values.ToImmutableList();
+    public ImmutableDictionary<string, int> Data => _data.ToImmutableDictionary();
+
+    public void Store(string senderId, int data)
+    {
+        // Fake operational time
+        Thread.Sleep(RandomGenerator.Next(10));
+        if (RandomGenerator.Next(2) == 1)
+        {
+            _data[$"{senderId}-{DateTime.Now:s}-{_dataCounter}"] = data;
+            Interlocked.Increment(ref _dataCounter);
+            Console.Write("+");
+            return;
+        }
+        else
+        {
+            Console.Write('-');
+            return;
+        }
+    }
+}
---
+++ personnel/transat/Transat/Program.cs
@@ -0,0 +1,35 @@
+﻿using Transat;
+
+const int iterations = 500;
+
+FaillibleQos0Storage storage1 = new();
+FaillibleQos0Storage storage2 = new();
+
+var publishers = new List<NonResilientPublisher>
+{
+    new(storage1, storage2),
+    new(storage1, storage2),
+    new(storage1, storage2)
+};
+
+// Liste des tâches
+var tasks = new List<Task>();
+
+for (var i = 0; i < iterations; i++)
+{
+    foreach (var publisher in publishers)
+    {
+        tasks.Add(Task.Run(() => publisher.Send(1)));
+    }
+}
+
+// Attendre toutes les tâches
+Task.WaitAll(tasks.ToArray());
+
+// Compute and show results
+int sum1 = storage1.Values.Sum();
+int sum2 = storage2.Values.Sum();
+
+Console.WriteLine($"\nTotal expected : {iterations * publishers.Count}");
+Console.WriteLine($"Sum storage1: {sum1}");
+Console.WriteLine($"Sum storage2: {sum2}");
